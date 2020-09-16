2e166255668d0d6147445dd518f7c927b235b049
Volha Borys <olgaboris94@gmail.com>
2020-09-16 21:31:52 +0300

    add email to list

--- src/components/emails-input/emails-input.ts
+++ src/components/emails-input/emails-input.ts
@@ -1,10 +1,23 @@
 class EmailsInput {
-    private container: HTMLElement;
+    private root: HTMLElement;
 
-    constructor(container: HTMLElement) {
-        this.container = container;
+    private containerEl: HTMLElement;
+
+    private listEl: HTMLElement;
+
+    private inputEl: any;
+
+    private state: any;
+
+    constructor(root: HTMLElement) {
+        this.root = root;
+        this.state = {
+            emailList: [],
+        };
 
         this.render();
+        this.queryElements();
+        this.subscribe();
     }
 
     static markup() {
@@ -16,8 +29,74 @@ class EmailsInput {
         `;
     }
 
+    static emailMarkup(email: string) {
+        return `
+                <span class="email">${email}</span>
+                <span class="icon-remove"></span>
+        `;
+    }
+
     render() {
-        this.container.innerHTML = EmailsInput.markup();
+        this.root.innerHTML = EmailsInput.markup();
+    }
+
+    queryElements() {
+        this.containerEl = this.root.querySelector('.emails-input');
+        this.listEl = this.root.querySelector('.email-list');
+        this.inputEl = this.root.querySelector('.email-add-more');
+    }
+
+    static isEmailValid(email: string): boolean {
+        const regexp = /\S+@\S+\.\S+/;
+
+        return regexp.test(email.toLowerCase());
+    }
+
+    addEmail(value: string) {
+        if (!value) return;
+
+        const el = document.createElement('span');
+
+        const validationClass = EmailsInput.isEmailValid(value)
+            ? 'valid'
+            : 'invalid';
+        el.classList.add('email-list-item');
+        el.classList.add(validationClass);
+
+        el.innerHTML = EmailsInput.emailMarkup(value);
+        this.listEl.appendChild(el);
+    }
+
+    onContainerClick(e: Event) {
+        if (e.target === this.containerEl) {
+            this.inputEl.focus();
+        }
+    }
+
+    onKeydown(e: KeyboardEvent) {
+        if (e.key === ',') {
+            const email = this.inputEl.value.slice(0, -1);
+            this.addEmail(email);
+            this.inputEl.value = '';
+        } else if (e.key === 'Enter') {
+            this.addEmail(this.inputEl.value);
+            this.inputEl.value = '';
+        }
+    }
+
+    onBlur(e: Event) {
+        this.addEmail(this.inputEl.value);
+        this.inputEl.value = '';
+    }
+
+    subscribe() {
+        this.inputEl.addEventListener('keyup', this.onKeydown.bind(this));
+        this.inputEl.addEventListener('blur', this.onBlur.bind(this));
+
+        this.containerEl.addEventListener(
+            'click',
+            this.onContainerClick.bind(this),
+        );
     }
 }
 
